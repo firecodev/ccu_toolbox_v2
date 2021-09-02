3d6adee73e1ed1540f5d9244a29b0e94b16c8f2a
Firecodev <59217572+firecodev@users.noreply.github.com>
2021-09-02 17:18:36 +0800

    [Update] Update version info

--- lib/environments/app_info.dart
+++ lib/environments/app_info.dart
@@ -1,5 +1,5 @@
-int get buildVersion => 20210608;
-String get version => '2.0.2';
-String get buildDate => '2021/06/08';
+int get buildVersion => 20210902;
+String get version => '2.0.3';
+String get buildDate => '2021/09/02';
 String get flutterVersion => '1.22.5';
-String get scheduleVersion => '2021/02/21';
+String get scheduleVersion => '2021/09/02';
