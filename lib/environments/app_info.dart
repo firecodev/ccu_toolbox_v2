ea09b45248782dba03a7b459892e0cddc179dffa
Firecodev <59217572+firecodev@users.noreply.github.com>
2022-03-05 23:16:52 +0800

    [Version] Bump version to 2.0.4

--- lib/environments/app_info.dart
+++ lib/environments/app_info.dart
@@ -1,5 +1,5 @@
-int get buildVersion => 20210902;
-String get version => '2.0.3';
-String get buildDate => '2021/09/02';
+int get buildVersion => 20220305;
+String get version => '2.0.4';
+String get buildDate => '2022/03/05';
 String get flutterVersion => '1.22.5';
 String get scheduleVersion => '2021/09/02';
